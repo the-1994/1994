61cf6be96b74b93b2d8ec4064989368be48041aa
hifilorau <tomrau@gmail.com>
2015-07-07 00:05:51 -0400

    checkin social

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,8 +2,11 @@ app.hamburger = function(){
 	
 	$( '.menu-btn' ).click(function(){
     	// $('.responsive-menu').toggleClass('expand');
-    	$('.responsive-menu').toggle("slide", { direction: "down" }, 300);
+    	$('.responsive-menu').toggle("slide", { direction:"down" }, 300);
     	 // $('.flags').toggle("slide", { direction: "left" }, 300);
-    	// $('.hero-section').toggleClass('expand400');
+    	$('.hero-section').toggleClass('expand400');
+    	// $('.philosophy').toggleClass('expand400');
+    	$('.contact-content').toggleClass('expand400');
+
     	     });
 };
