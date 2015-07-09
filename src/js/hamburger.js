880f83d7de1e30ecc20036f3e12b39fefe67ec6f
hifilorau <tomrau@gmail.com>
2015-07-09 11:38:45 -0400

    checkin

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,12 +1,26 @@
 app.hamburger = function(){
 	
-	$( '.menu-btn' ).click(function(){
-    	// $('.responsive-menu').toggleClass('expand');
-    	$('.responsive-menu').toggle("slide", { direction:"down" }, 300);
-    	 // $('.flags').toggle("slide", { direction: "left" }, 300);
-    	$('.hero-section').toggleClass('expand400');
-    	// $('.philosophy').toggleClass('expand400');
-    	$('.contact-content').toggleClass('expand400');
+	
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+		
+		if ($('.responsive-menu').hasClass('slideDown')) {
+		
+			   $('.responsive-menu').addClass('fadeOutUp');
+				 setTimeout(function () {
+							$('.responsive-menu').removeClass('slideDown');
+					}, 500);
 
-    	     });
+		}	else {
+			$('.responsive-menu').removeClass('fadeOutUp');
+			$('.responsive-menu').addClass('slideDown');
+				}
+//    	 // $('.flags').toggle("slide", { direction: "left" }, 300);
+//    	$('.hero-section').toggleClass('expand400');
+//    	// $('.philosophy').toggleClass('expand400');
+//    	$('.contact-content').toggleClass('expand400');
+  });
+	
+
+	
 };
